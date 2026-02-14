afbc34d5393122ad1056c105d8c9e9b8e7ec39f6
Sarthak Agarwal <sarthakroxxx99@gmail.com>
2026-02-14 12:43:48 +0530

    added db schema

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from 'better-auth'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+import { prisma } from '@/lib/db'
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  plugins: [tanstackStartCookies()],
+})
